a2f5c33d3b31ada0a4bb831d28be00d1c5833715
David Kearney <15169235@studentmail.ul.ie>
2017-11-16 12:01:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // write your code below!
-define function happyHolidays(){
-  return "Happy holidays!"
+function happyHolidays(){
+  return "Happy holidays!";
 }
 
-define function happyHolidaysTo(var name){
- return 'Happy holidays, ${name}!'
+function happyHolidaysTo(var name){
+ return 'Happy holidays, ${name}!';
 }
 
-define function happyHolidayTo(var holiday, var name){
-  return 'Happy ${holiday}, ${name}!'
+function happyHolidayTo(var holiday, var name){
+  return 'Happy ${holiday}, ${name}!';
 }
 
-define function holidayCountdown(var holiday, var days){
-  return "It\'s ${days} days until ${holiday}!"
+function holidayCountdown(var holiday, var days){
+  return 'It\'s ${days} days until ${holiday}!';
 }
