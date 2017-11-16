927f9cd80eea68691caeb2998eea51e2395597ec
David Kearney <15169235@studentmail.ul.ie>
2017-11-16 11:57:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidays(){
 function happyHolidaysTo(string name){
  return 'Happy holidays, ${name}!'
 }
+
+function happyHolidayTo(String holiday, String name){
+  return 'Happy ${holiday}, ${name}!'
+}
